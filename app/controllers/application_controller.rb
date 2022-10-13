b9b59035b6a7b8744477008ebc83fe55479bf6fb
Zipporah <shemaiahm1@gmail.com>
2022-10-13 15:48:20 +0000

    game templates work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,58 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
-def play_rock
-  render({ :template => "game_templates/rock.html.erb"})
-  
-end
+  def play_rock
+# redirect_to("https://www.wikipedia.org")
+   index = rand(3)  
 
+   moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.at(index) 
+
+  if @comp_move == "rock" 
+    @outcome = "We tied!" 
+  elsif @comp_move  == "paper" 
+    @outcome = "We lost!" 
+  elsif @comp_move == "scissors" 
+    @outcome = "We won!"  
+ end
+
+    render({:template => "game_templates/user_rock.html.erb"})
+  end
+
+
+    def play_paper
+      index = rand(3)
+
+      moves = ["rock", "paper", "scissors"]
+
+        @comp_move = moves.at(index)
+
+      if @comp_move == "rock"
+        @outcome = "We won!"
+      elsif @comp_move == "paper"
+        @outcome = "We tied!"
+      elsif @comp_move == "scissors"
+        @outcome = "We lost!"
+      end
+    render({:template => "game_templates/user_paper.html.erb"})
+
+  end
+
+    def play_scissors
+      index = rand(3)
+
+      moves = ["rock", "paper", "scissors"]
+
+      @comp_move = moves.at(index)
+
+      if @comp_move == "rock"
+        @outcome = "We lost!"
+      elsif @comp_move == "paper"
+        @outcome = "We won!"
+      elsif @comp_move == "scissors"
+        @outcome = "We tied!"
+      end
+      render({:template => "game_templates/user_scissors.html.erb"})
+    end
 end
